c65cc9c17e3b7cbbd238467243e8285fd102bfa5
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2019-01-10 20:35:15 -0600

    Added id to TextView, creates a Function to generate a random number between 1 and 6, call function from MainActivity

--- app/src/main/java/com/example/victorolvera/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/victorolvera/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.victorolvera.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        //Generate random value between 1-6
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
